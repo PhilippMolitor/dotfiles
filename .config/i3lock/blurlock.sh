68feacea07fef8d153e75a2e6a1de0fc454f5342
Philipp Molitor <phil.x64@gmail.com>
2018-07-06 16:00:16 +0200

    fixed indentation

--- .config/i3lock/blurlock.sh
+++ .config/i3lock/blurlock.sh
@@ -8,12 +8,12 @@ RESOLUTION=$(xrandr --current | grep '*' | head -n 1 | awk '{print $1}')
 OVERLAY_IMG=$(find $OVERLAY_PATH -type f | rev | cut -d'/' -f1 | rev | shuf -n 1)
 
 ffmpeg -f x11grab \
-       -video_size $RESOLUTION \
-	   -y \
-	   -i $DISPLAY \
-	   -i "$OVERLAY_PATH/$OVERLAY_IMG" \
-	   -filter_complex "boxblur=15,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
-	   -vframes 1 $IMG_OUT
+	-video_size $RESOLUTION \
+	-y \
+	-i $DISPLAY \
+	-i "$OVERLAY_PATH/$OVERLAY_IMG" \
+	-filter_complex "boxblur=15,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
+	-vframes 1 $IMG_OUT
 
 i3lock -i $IMG_OUT
 \rm -f $IMG_OUT
