85cec41566ce11f581841d44b3992e3686279e21
PhilsLab <phil.x64@gmail.com>
2018-10-15 22:00:32 +0200

    add amd graphics info script to polybar

---
+++ .scripts/amdinfo/amdinfo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DEVICE="amdgpu-pci-0400"
+SENSOR="temp1"
+
+while true; do
+  temp="$(sensors $DEVICE 2>/dev/null | sed -En "s/^$SENSOR:.+[\+\-]([0-9]+\.[0-9]+).*\(.*$/\1/p")"
+
+  if [ -z "$temp" ]; then
+    echo "off"
+  else
+    echo "$temp°C"
+  fi
+
+  sleep 5;
+done;
+
